6886db2c3f283f87f341b21a2efd9cced6077cd5
Fei Li <feili4287@gmail.com>
2022-10-14 23:21:37 -0400

    add script

--- mdpreviewer/src/App.js
+++ mdpreviewer/src/App.js
@@ -1,9 +1,49 @@
 import "./App.css";
-
+import { useState } from "react";
+import { marked } from "marked";
+import React from "react";
 function App() {
+  const [text, setText] = useState(`
+  # H1
+  ## H2
+  ### H3
+  [title](https://www.example.com)
+  \`code\`
+  \`\`\`
+  {
+    "firstName": "John",
+    "lastName": "Smith",
+    "age": 25
+  }
+  \`\`\`
+  - First item
+  - Second item
+  - Third item
+  > blockquote
+  ![alt text](image.jpg)
+  **bold text**
+  `);
+  marked.setOptions({
+    breaks: true,
+  });
   return (
     <div className="App">
-      <header className="markdown-previewer"></header>
+      <textarea
+        id="editor"
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
+        value={text}
+        rows="10"
+        columns="200"
+      ></textarea>
+
+      <div
+        id="preview"
+        dangerouslySetInnerHTML={{
+          __html: marked.parse(text),
+        }}
+      ></div>
     </div>
   );
 }
